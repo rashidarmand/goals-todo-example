744f364a5d3784d6840d1613b1285b9e08dbaf69
Rashid H Armand <rashidharmand@gmail.com>
2019-01-27 23:02:26 -0500

    refactor list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,19 @@
 import React from 'react';
 
-export default function List (props) {
-  const { toggle, items, remove } = props;
-  return (
-    <ul>
-      {items.map((item) => (
-        <li key={item.id} style={{ cursor: 'pointer' }}>
-          <span
-            onClick={ () => toggle && toggle(item.id) }
-            style={{ textDecoration: item.complete ? 'line-through' : 'none' }}
-          >
-            {item.name}
-          </span>
-          <button onClick={ () => remove(item) }>X</button>
-        </li>
-      ))}
-    </ul>
-  )
-}
+const List = ({ toggle, items, remove }) => (
+  <ul>
+    {items.map((item) => (
+      <li key={item.id} style={{ cursor: 'pointer' }}>
+        <span
+          onClick={ () => toggle && toggle(item.id) }
+          style={{ textDecoration: item.complete ? 'line-through' : 'none' }}
+        >
+          {item.name}
+        </span>
+        <button onClick={ () => remove(item) }>X</button>
+      </li>
+    ))}
+  </ul>
+)
+
+export default List;
